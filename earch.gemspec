daf2d3e06aca825a212f4402a41d1e1610c3f863
flum1025 <flum.1025@gmail.com>
2017-08-10 22:56:55 +0900

    files

--- earch.gemspec
+++ earch.gemspec
@@ -9,19 +9,10 @@ Gem::Specification.new do |spec|
   spec.authors       = ["flum1025"]
   spec.email         = ["flum.1025@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Twitter timeline notify}
+  spec.description   = %q{This application monitors the Twitter timeline and notifies Slack according to the conditions.}
+  spec.homepage      = "https://github.com/flum1025/earch"
   spec.license       = "MIT"
-
-  # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
-  # to allow pushing to a single host or delete this section to allow pushing to any host.
-  if spec.respond_to?(:metadata)
-    spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
-  else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
-  end
 
   spec.files         = `git ls-files -z`.split("\x0").reject do |f|
     f.match(%r{^(test|spec|features)/})
@@ -32,4 +23,9 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.15"
   spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "pry"
+  spec.add_dependency 'twitter'
+  spec.add_dependency 'twitter-stream-patch'
+  spec.add_dependency 'hashie'
+  spec.add_dependency 'slack-ruby-client'
 end
--- lib/earch.rb
+++ lib/earch.rb
@@ -1,5 +1,61 @@
 require "earch/version"
+require "earch/rule"
+require "twitter"
+require "twitter-stream-patch"
+require "hashie"
+require "slack-ruby-client"
+require "optparse"
 
-module Earch
-  # Your code goes here...
+class Earch
+  def initialize(file)
+    @setting = Hashie::Mash.load(file)
+    @rules = Rules.new(@setting.rules)
+    @stream_client = Twitter::Streaming::Client.new @setting.twitter.api_key
+    @slack_client = Slack::Web::Client.new(@setting.slack.api_key)
+  end
+
+  def start
+    @stream_client.user(@setting.twitter.stream_options) do |object|
+      case object
+      when Twitter::Tweet
+        execute(object)
+      end
+    end
+  end
+
+  private
+  def execute(object)
+    if @rules.match?(object)
+      notify(object)
+    end
+  end
+
+  def notify(object)
+    @slack_client.chat_postMessage(
+      icon_emoji: @setting.slack.user_icon,
+      username: 'Earch',
+      channel: @setting.slack.notify_channel,
+      attachments: notify_content(object)
+    )
+  end
+
+  def notify_content(object)
+    [
+      {
+        "fallback": "#{object.text} by @#{object.user.screen_name}",
+        "color": "#4169e1",
+        "author_name": "#{object.user.name}(@#{object.user.screen_name})",
+        "author_link": object.user.url,
+        "author_icon": object.user.profile_image_url_https,
+        "text": object.text,
+        "fields": [
+          {
+            "value": "<#{object.url}|Tweet>",
+          }
+        ],
+        "footer": "#{@setting.slack.icon} #{@setting.twitter.screen_name}",
+        "ts": object.created_at.to_f
+      }
+    ]
+  end
 end
